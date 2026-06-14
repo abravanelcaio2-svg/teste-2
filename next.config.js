095fe2e171bf967438cb6160ce449fdd3c3fefd4
francabusinesstme-cmd <francabusinesstme@gmail.com>
2026-06-13 22:14:28 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     remotePatterns: [
       { protocol: 'http',  hostname: 'localhost' },
-      { protocol: 'http',  hostname: '2.24.98.43' },
-      { protocol: 'https', hostname: '2.24.98.43' },
       { protocol: 'https', hostname: 'pietset.space' },
       { protocol: 'http',  hostname: 'pietset.space' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
